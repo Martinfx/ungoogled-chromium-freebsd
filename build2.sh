19940b5e243b4cc90a1ee42dbc8c24476ebc982d
M <1999714+Martinfx@users.noreply.github.com>
2021-01-09 10:27:03 +0100

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -5,7 +5,7 @@
 
 _root_dir=$(dirname $(readlink -f $0))
 #_download_cache="$_root_dir/build/download_cache"
-#_src_dir="$_root_dir/build/src"
+_src_dir="$_root_dir/build/src"
 #_main_repo="$_root_dir/ungoogled-chromium"
 
 #rm -rf "$_src_dir" || true
